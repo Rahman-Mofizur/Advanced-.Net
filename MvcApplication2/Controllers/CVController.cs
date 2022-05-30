32d7b2510b834c18db73b800e8b375dc14d947d7
Mohammed Eiman <mohammedeiman0177@gmail.com>
2022-05-30 23:36:58 +0600

    Lab Task-01

---
+++ MvcApplication2/Controllers/CVController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class CVController : Controller
+    {
+        //
+        // GET: /CV/
+
+        public ActionResult Introduction()
+        {
+            return View();
+        }
+        public ActionResult ContactInfo()
+        {
+            return View();
+        }
+        public ActionResult Language()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Interests()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+
+    }
+}
